89477d4cb4b13143c34a8948821a3fd04e3bdd45
Stanislav Ochotnicky <sochotnicky@redhat.com>
2011-12-04 16:22:59 +0100

    Start handling arguments properly with argparser

--- mbank2qif.py
+++ mbank2qif.py
@@ -18,12 +18,8 @@
 
 from contextlib import nested
 import codecs
-import sys
 import csv
-
-if len(sys.argv) != 3:
-    print "Usage: %s input_file.csv output_file.qif" % sys.argv[0]
-    sys.exit(1)
+import argparse
 
 def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
     # csv.py doesn't do Unicode; encode temporarily as UTF-8:
@@ -37,33 +33,48 @@ def utf_8_encoder(unicode_csv_data):
     for line in unicode_csv_data:
         yield line.encode('utf-8')
 
-with nested(open(sys.argv[1], 'r'), open(sys.argv[2], 'w')) as (input, output):
-    reader = codecs.getreader("cp1250")
-    writer = codecs.getwriter("utf-8")
+def convert(infile, outfile):
+    with nested(open(infile, 'r'), open(outfile, 'w')) as (input, output):
+        reader = codecs.getreader("cp1250")
+        writer = codecs.getwriter("utf-8")
 
-    inputreader = reader(input)
-    outputwriter = writer(output)
+        inputreader = reader(input)
+        outputwriter = writer(output)
 
-    items = 0
-    outputwriter.write("!Type:Bank\n")
-    for row in unicode_csv_reader(inputreader.readlines(), delimiter=';'):
-        if items == 0 and len(row) > 0 and row[0] == u"#Datum uskutečnění transakce":
-            items = 1
-            continue
-        if items == 1:
-            if len(row) == 0:
-                break
-            date = row[1]
-            d, m, y = date.split('-')
-            outputwriter.write(u"D%s/%s/%s\n" % (m, d, y))
-            outputwriter.write(u"T%s\n" % row[9].replace(',','.'))
-            trans_type = row[2].replace('\'','').replace('"','').strip()
-            trans_desc = row[3].replace('\'','').replace('"','').strip()
-            trans_target = row[4].replace('\'','').replace('"','').strip()
-            trans_acc = row[5].replace('\'','').replace('"','').strip()
-            outputwriter.write(u"M%s %s %s %s\n" %  (trans_type,
-                                                        trans_desc,
-                                                        trans_target,
-                                                        trans_acc) )
-            outputwriter.write(u'^\n')
+        items = 0
+        outputwriter.write("!Type:Bank\n")
+        for row in unicode_csv_reader(inputreader.readlines(), delimiter=';'):
+            if items == 0 and len(row) > 0 and row[0] == u"#Datum uskutečnění transakce":
+                items = 1
+                continue
+            if items == 1:
+                if len(row) == 0:
+                    break
+                date = row[1]
+                d, m, y = date.split('-')
+                outputwriter.write(u"D%s/%s/%s\n" % (m, d, y))
+                outputwriter.write(u"T%s\n" % row[9].replace(',','.'))
+                trans_type = row[2].replace('\'','').replace('"','').strip()
+                trans_desc = row[3].replace('\'','').replace('"','').strip()
+                trans_target = row[4].replace('\'','').replace('"','').strip()
+                trans_acc = row[5].replace('\'','').replace('"','').strip()
+                outputwriter.write(u"M%s %s %s %s\n" %  (trans_type,
+                                                            trans_desc,
+                                                            trans_target,
+                                                            trans_acc) )
+                outputwriter.write(u'^\n')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Bank statement to QIF file converter')
+    parser.add_argument('-i', '--input',
+                        help='input file to process [default:stdin]',
+                        default='/dev/stdin')
+    parser.add_argument('-o', '--output',
+                        help='output file [default:stdout]',
+                        default='/dev/stdout')
+    parser.add_argument('-t', '--type',
+                        help='Type of input file [default:mbank]',
+                        default='mbank')
+    args = parser.parse_args()
+    convert(args.input, args.output)
 
